15f8af338247104f50b5c19e7ab0f6710235cdd6
Andrew Lee <andrewlee@CACPWEB-ANDREWL.local>
2015-04-04 17:25:58 -0700

    fixing things

--- DcsnMkr/config/routes.rb
+++ DcsnMkr/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  root :to => 'home#index'
+  root :to => 'home#login'
+  namespace :home do
+    get :index
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
